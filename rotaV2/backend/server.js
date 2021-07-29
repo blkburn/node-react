5a6da9a5d3e9df0a6f7cbe1659f81f8ea09af5b3
craig blackburn <craigb211@gmail.com>
2021-07-29 14:39:22 +0100

    update for server

--- rotaV2/backend/server.js
+++ rotaV2/backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
-// import path from 'path'
+import path from 'path'
 import colors from 'colors'
 import morgan from 'morgan'
 // import { notFound, errorHandler } from './middleware/errorMiddleware.js'
@@ -12,6 +12,12 @@ import rotaRoutes from './routes/rotaRoutes.js'
 
 // import orderRoutes from './routes/orderRoutes.js'
 // import uploadRoutes from './routes/uploadRoutes.js'
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 dotenv.config()
 
@@ -50,10 +56,10 @@ app.use(function (err, req, res, next) {
 })
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'))
   )
 } else {
   app.get('/', (req, res) => {
